55756bb3b8178ae25e8ec6a1de798981a42f9978
cryptactics <ecoadaptivetactic360@protonmail.com>
2022-10-05 09:26:16 -0400

    Initial commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x2F378590e07982087c5562516e44Db7fB8736865";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
